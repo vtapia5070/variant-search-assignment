a9390c0c96889e0ff3150011cfc16902f8420863
Victoria <tapia.victoria5070@gmail.com>
2019-01-02 10:05:51 -0800

    organize file structure, code cleanup

--- server/app.js
+++ server/app.js
@@ -19,7 +19,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/genes', routes);
