373fe21d8bf95a7db205c9198318e06c470f7629
Victoria <tapia.victoria5070@gmail.com>
2019-01-02 09:27:30 -0800

    code cleanup

--- client/src/SearchBar/SearchBar.js
+++ client/src/SearchBar/SearchBar.js
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import SearchSuggestions from '../SearchSuggestions/SearchSuggestions.js';
 import './SearchBar.css';
 
-class SearchBar extends Component {
-
-  renderSuggestedSearchItems () {
-    return this.props.geneMatches.map((gene) => (
-      <div 
-        className="suggestedItem" 
-        key={gene.geneName}
-        onClick={this.props.selectGene}
-      >
-        {gene.geneName}
-      </div>
-    ));
-  }
-
-  render() {
-    return (
-      <div>
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          // className={classes.textField}
-          // TODO: add value placeholder for selected option
-          margin="normal"
-          onChange={this.props.onChange}
-        />
-        {
-          !this.props.isSuggestionListHidden && (
-            <SearchSuggestions 
-            suggestions={this.props.geneMatches} 
-            handleClick={this.props.selectGene} 
-            />
-          )
-        }
-      </div>
-    );
-  }
+const SearchBar = (props) => {
+  return (
+    <div>
+      <TextField
+        id="standard-search"
+        label="Search field"
+        type="search"
+        margin="normal"
+        onChange={props.onChange}
+      />
+      {
+        !props.isSuggestionListHidden && (
+          <SearchSuggestions 
+          suggestions={props.geneMatches} 
+          handleClick={props.selectGene} 
+          />
+        )
+      }
+    </div>
+  );
 }
 
 export default SearchBar;
--- client/src/SearchSuggestions/SearchSuggestions.js
+++ client/src/SearchSuggestions/SearchSuggestions.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-// import Divider from '@material-ui/core/Divider';
 
 const SearchSuggestions = (props) => {
   const listItems = props.suggestions.map((suggestion) => {
